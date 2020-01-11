b6bf8420ca9e6fc698764ce121433dd6bb179b9b
byteconde <iamdeyvi@gmail.com>
2020-01-11 13:06:48 -0500

    Add main component & searchQuery useState hook

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Weather.css";
+import WeatherInfo from "../components/WeatherInfo";
 
 function Weather() {
+  const API_KEY = "63374cfea606656fd08c6a5045d09eef";
+  const [searchQuery, setSearchQuery] = useState();
+  const [weatherData, setWeatherData] = useState({
+    temp: null,
+    humidity: null,
+    desc: null,
+    city: null
+  });
+  const [isValidCity, setIsValidCity] = useState(true);
+
+  const updateSearchQuery = event => {
+    let city = event.target.value;
+    let isValid = validCity(city);
+    setSearchQuery(city);
+
+    if (isValid || city === "") {
+      setIsValidCity(true);
+    } else {
+      setIsValidCity(false);
+    }
+  };
+
+  const validCity = city => {
+    let regex = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
+    return regex.test(city);
+  };
+
+  const getWeatherData = async () => {
+    if (!validCity || searchQuery === "") {
+      setIsValidCity(false);
+      return;
+    }
+    await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&lang=sp&appid=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(data =>
+        setWeatherData({
+          temp: KelvinToCelsius(data.main.temp),
+          humidity: data.main.humidity,
+          desc: data.weather[0].main,
+          city: data.name
+        })
+      );
+  };
+  const KelvinToCelsius = temp => {
+    return (temp - 273.15).toFixed(0);
+  };
+
   return (
     <section className="weather-container">
       <header className="weather-header">
-        <div></div>
+        <h3>
+          {" "}
+          Weatherify
+          <span aria-label="emoji-rain" role="img">
+            ⚡️
+          </span>
+        </h3>
+        <div>
+          <input
+            placeholder="Enter your city"
+            className="search-input"
+            onChange={updateSearchQuery}
+            maxLength="60"
+          />
+          <button className="material-icons" onClick={getWeatherData}>
+            search
+          </button>
+        </div>
       </header>
+      <p className="error">{isValidCity ? "" : "Invalid City"}</p>
+      <section className="weather-info">
+        {weatherData.temp === null ? (
+          <p>
+            No weather to display <i className="material-icons">wb_sunny</i>
+          </p>
+        ) : (
+          <WeatherInfo data={weatherData} />
+        )}
+      </section>
     </section>
   );
 }
