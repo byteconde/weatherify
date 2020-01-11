70dd4b4f9b3c17a85d62d0b1103ff0d10a1b0de3
byteconde <iamdeyvi@gmail.com>
2020-01-11 03:18:47 -0500

    Modify boilerplate & add weather container component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <section className="app-container">
+      <Weather />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Weather.css";
+
+function Weather() {
+  return (
+    <section className="weather-container">
+      <header className="weather-header">
+        <div></div>
+      </header>
+    </section>
+  );
+}
+
+export default Weather;
